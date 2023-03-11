1cdf919c8c1829dab42af9c938626dbc5b18f0bb
Rashonda Edwards <rashondaedwards@aol.com>
2023-03-11 12:50:22 -0800

    Final-Product

--- move.js
+++ move.js
@@ -6,7 +6,64 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+                            // 11. changed the callback to handleDirectionChange
+    function moveWithArrowKeys(left, bottom, handleDirectionChange){
+        // 10. this code was moved from index.js and replaced character with element
+        let direction = null;
+        let x = left; 
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+      
+        function moveCharacter(){
+            if (direction === 'west') {
+                x-=1
+            }
+            if (direction === 'north') {
+                y+=1
+            }
+            if (direction === 'east') {
+                x+=1
+            }
+            if (direction === 'south') {
+                y-=1
+            }
+           
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+
+        setInterval(moveCharacter, 1)
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            // 11. changed the callback to handleDirectionChange
+            handleDirectionChange(direction)
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+            // 11. changed the callback to handleDirectionChange
+            handleDirectionChange(direction)
+        })
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
--- somenotes.js
+++
@@ -1,6 +0,0 @@
-let users = [
-    { id: 1, firstName: 'Azami', lastName: 'Bazia', age: 18 },
-    { id: 2, firstName: 'Elliot', lastName: 'Noor', age: 23 },
-    { id: 3, firstName: 'Samir', lastName: 'Zahara', age: 7 },
-    { id: 4, firstName: 'Idris', lastName: 'Christenson', age: 16 },
-]
